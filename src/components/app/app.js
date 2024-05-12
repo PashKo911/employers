4c61c612188d47c191e05773c3e012fb9d7055c9
Pavel <rabota.pashok@gmail.com>
2024-05-12 22:53:57 +0300

    ls 136 compleated

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,44 +1,92 @@
+import { Component } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
 import AppFilter from '../app-filter/app-filter'
 import AppInfo from '../app-info/app-info'
 import EmpoyersList from '../employers-list/employers-list'
 import SearchPanel from '../search-panel/search-panel'
 import EmployersAddForm from '../employers-add-form/employers-add-form'
+
 import './app.css'
 
-function App() {
-	const data = [
-		{
-			name: 'Pasha',
-			salary: 800,
-			increase: false,
-			id: 1,
-		},
-		{
-			name: 'Ira',
-			salary: 3000,
-			increase: true,
-			id: 2,
-		},
-		{
-			name: 'Svyatoslav',
-			salary: 5000,
-			increase: false,
-			id: 3,
-		},
-	]
+class App extends Component {
+	constructor(props) {
+		super(props)
 
-	return (
-		<div className="app">
-			<AppInfo />
-			<div className="search-panel">
-				<SearchPanel />
-				<AppFilter />
+		this.state = {
+			data: [
+				{
+					name: 'Pasha',
+					salary: 800,
+					increase: false,
+					id: 1,
+				},
+				{
+					name: 'Ira',
+					salary: 3000,
+					increase: true,
+					id: 2,
+				},
+				{
+					name: 'Svyatoslav',
+					salary: 5000,
+					increase: false,
+					id: 3,
+				},
+			],
+		}
+	}
+
+	deleteItem = (id) => {
+		this.setState(({ data }) => {
+			return {
+				data: data.filter((element) => element.id !== id),
+			}
+		})
+	}
+
+	addItem = (name, salary) => {
+		this.setState(({ data }) => ({
+			data: [
+				...data,
+				{
+					name,
+					salary,
+					increase: false,
+					id: uuidv4(),
+				},
+			],
+		}))
+	}
+
+	onToggleIncrease = (id) => {
+		console.log(`Increase this ${id}`)
+	}
+	onToggleRice = (id) => {
+		console.log(`Rice this ${id}`)
+	}
+
+	render() {
+		const { data } = this.state
+
+		return (
+			<div className="app">
+				<AppInfo />
+				<div className="search-panel">
+					<SearchPanel />
+					<AppFilter />
+				</div>
+
+				<EmpoyersList
+					data={data}
+					onDelete={this.deleteItem}
+					onToggleIncrease={this.onToggleIncrease}
+					onToggleRice={this.onToggleRice}
+				/>
+				<EmployersAddForm onAdd={this.addItem} />
 			</div>
-
-			<EmpoyersList data={data} />
-			<EmployersAddForm />
-		</div>
-	)
+		)
+	}
 }
 
 export default App
--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -1,10 +1,18 @@
 import EmployeesListItem from '../employers-list-item/employers-list-item'
 import './employers-list.css'
 
-const EmpoyersList = ({ data }) => {
+const EmpoyersList = ({ data, onDelete, onToggleIncrease, onToggleRice }) => {
 	const elements = data.map((item) => {
 		const { id, ...itemProps } = item
-		return <EmployeesListItem key={id} {...itemProps} />
+		return (
+			<EmployeesListItem
+				key={id}
+				{...itemProps}
+				onDelete={() => onDelete(id)}
+				onToggleIncrease={() => onToggleIncrease(id)}
+				onToggleRice={() => onToggleRice(id)}
+			/>
+		)
 	})
 	return <ul className="app-list list-group">{elements}</ul>
 }
